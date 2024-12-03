331f225d1138fbec16fc3181f7cec0f03076d159
dndtjr03 <lee01seok@naver.com>
2024-12-03 21:15:04 +0900

    1. main파일 콘솔창 제목 추가 2. 캐릭터 디자인 및 색상 추가 - Char.h => 캐릭터 색상 매크로 및 캐릭터 생성 함수 추가 - Char.c => 캐릭터 디자인 추가, 캐릭터 색상 변경 함수 및 매크로 추가하여 완성

--- 기말대체과제/Char.h
+++ 기말대체과제/Char.h
@@ -1,1 +1,26 @@
 #pragma once
+#ifndef GAME_H
+#define GAME_H
+
+// 캐릭터와 적의 크기 정의
+#define CHARACTER_WIDTH 11
+#define CHARACTER_HEIGHT 7
+
+/*
+* ANSI 색상 코드 매크로
+* \033[색상코드;색상코드m
+* => C/C++에서 printf사용했을 때 문자열 색상 출력하는 명령어
+*/ 
+
+#define RESET_COLOR "\033[0m"		// 색상 초기화
+#define COLOR_WHITE "\033[37m"		// 색상 == 하얀색 (내 캐랙터 색상)
+#define COLOR_RED   "\033[31m"		// 색상 == 빨간색 (적 캐릭터 색상)
+
+// 색상 설정 매크로
+#define SET_COLOR(color) printf(color)
+
+// 캐릭터와 적을 출력하는 함수 선언
+void draw_character();
+void draw_enemy();
+
+#endif // GAME_H
